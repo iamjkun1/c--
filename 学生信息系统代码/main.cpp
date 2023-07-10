8969bcea8883ad64f58938a001e2d0df49cc3480
Jkun <3291310075@qq.com>
2023-07-10 21:16:49 +0800

    学生信息管理系统

---
+++ 学生信息系统代码/main.cpp
@@ -0,0 +1,119 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "main.h"
+
+////主菜单；
+
+struct Node* list = NULL;
+void systemMenu() {
+
+	printf("************************************************************************************************************\n");
+
+	printf("\t\t0.退出系统\n");
+	printf("\t\t1.输入（插入）信息\n");
+	printf("\t\t2.输出信息\n");
+	printf("\t\t3.删除信息\n");
+	printf("\t\t4.修改信息\n");
+	printf("\t\t5.查找信息\n");
+	printf("************************************************************************************************************\n");
+	printf("请输入0-5：");
+
+}
+
+void keyDown() {
+	int userKey;
+	scanf("%d", &userKey);
+	struct student tempdata;
+
+	switch (userKey) {
+
+	case 0:
+		printf("谢谢使用！\n");
+		system("pause");
+		exit(0);
+		break;
+	case 1:
+		printf("***************[插入信息]***************\n");
+		printf("学号 姓名 性别 班级 语文 数学 英语：\n");
+		scanf("%s%s%s%d%f%f%f",tempdata.num, tempdata.name, tempdata.sex,
+			&tempdata.cla, &tempdata.Chinese, &tempdata.Math, &tempdata.English);
+
+
+		insertNodeByHead(list, tempdata);
+
+		saveInfoFromFile("student.txt", list);
+
+		break;
+	case 2:
+		printf("***************[浏览信息]***************\n");
+		printList(list);
+
+		break;
+	case 3:
+		printf("***************[删除信息]***************\n");
+		printf("请input删除学生的姓名：\n");
+
+
+		scanf("%s", tempdata.name);
+		deleteNodeByAppoinName(list, tempdata.name);
+
+		saveInfoFromFile("student.txt", list);
+		break;
+	case 4:
+		printf("***************[修改信息]***************\n");
+		printf("输入要修改学生的学号：");
+		scanf("%s", tempdata.num);
+
+		if (searchNodeByAppoinNum(list, tempdata.num) == NULL) {
+			printf("No\n");
+		}
+		else {
+
+			struct Node* cutNode = searchNodeByAppoinNum(list, tempdata.num);
+
+			printf("input新的学生的学号，姓名，性别，班级，语文，数学，英语\n");
+
+				scanf("%s%s%s%d%f%f%f", cutNode->data.num, cutNode->data.name, cutNode->data.sex, &cutNode->data.cla,
+					&cutNode->data.Chinese, &cutNode->data.Math, &cutNode->data.English);
+
+		}
+		saveInfoFromFile("student.txt", list);
+
+		break;
+
+	case 5:
+		printf("***************[查找信息]***************\n");
+		printf("请输入要查找学生的学号：\n");
+		scanf("%s", tempdata.num);
+
+		if (list->next == NULL)printf("当前通讯录信息为空!\n");
+		else if (searchNodeByAppoinNum(list, tempdata.num) == NULL) {
+			printf("No!\n");
+		}
+		else {
+			printNode(searchNodeByAppoinNum(list, tempdata.num));
+		}
+		break;
+	default:
+		printf("选择错误，重新输入\n");
+		system("pause");
+		break;
+	}
+}
+
+
+int main() {
+
+	list = createList();
+	readInfoFromFile("student.txt", list);
+
+
+	while (1) {
+		systemMenu();
+		keyDown();
+		system("pause");
+		system("cls");
+	}
+	return 0;
+}
+
+
---
+++ 学生信息系统代码/main.h
@@ -0,0 +1,151 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+struct student {
+	char num[10];   ///学号
+	char name[5];   ///姓名
+	char sex[4];   ///性别
+	int cla;     ///班别
+	float Chinese, Math, English;   ///语文， 数学，英语；
+
+};
+
+struct Node {
+	//int data;
+	struct student data;
+	struct Node* next;
+};
+
+
+//创建链表；
+struct Node* createList() {
+
+	struct Node* listHeadNode = (struct Node*)malloc(sizeof(struct Node));
+	listHeadNode->next = NULL;
+	return listHeadNode;
+}
+
+//创建结点；
+struct Node* createNode(struct student data) {
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	newNode->data = data;
+	newNode->next = NULL;
+	return newNode;
+
+}
+
+//插入；
+void insertNodeByHead(struct Node* listHeadNode, struct student data) {
+	struct Node* newNode = createNode(data);
+	newNode->next = listHeadNode->next;
+	listHeadNode->next = newNode;
+}
+
+
+//删除；
+void deleteNodeByAppoinName(struct Node* listHeadNode,const char* name) {
+
+	struct Node* posFrontNode = listHeadNode;
+	struct Node* posNode = listHeadNode->next;
+	if (posNode == NULL) {
+		printf("数据为空，无法删除！\n");
+		return;
+	}
+	else {
+
+		while (strcmp(posNode->data.name, name)) {
+			posFrontNode = posNode;
+			posNode = posFrontNode->next;
+			if (posNode == NULL) {
+				printf("数据为空，无法删除！\n");
+				return;
+			}
+
+		}
+		posFrontNode->next = posNode->next;
+		free(posNode);
+
+	}
+
+}
+
+
+//查找；
+struct Node* searchNodeByAppoinNum(struct Node* listHeadNode,const char* num) {
+	struct Node* pMove = listHeadNode->next;
+	if (pMove == NULL) {
+		return pMove;
+	}
+	else {
+		while (pMove) {
+			if(!strcmp(pMove->data.num, num)) {
+				return pMove;
+			}
+			pMove = pMove->next;
+		}
+		printf("无法找到该信息!\n");
+		return NULL;
+	}
+}
+
+
+
+void printNode(struct Node* curNode) {
+	printf("学号\t姓名\t性别\t班级\t语文\t数学\t英语\n");
+	printf("%s\t%s\t%s\t%d\t%.2f\t%.2f\t%.2f\n",curNode->data.num,curNode->data.name, curNode->data.sex,
+		curNode->data.cla, curNode->data.Chinese, curNode->data.Math, curNode->data.English);
+
+}
+
+
+
+////打印链表,遍历；
+void printList(struct Node* listHeadNode) {
+
+	struct Node* pMove = listHeadNode->next;
+	printf("学号\t姓名\t性别\t班级\t语文\t数学\t英语\n");
+
+	while (pMove) {
+		printf("%s\t%s\t%s\t%d\t%.2f\t%.2f\t%.2f\n", pMove->data.num,pMove->data.name,pMove->data.sex,pMove->data.cla,
+		pMove->data.Chinese,pMove->data.Math, pMove->data.English);
+
+
+		pMove = pMove->next;
+	}
+	printf("\n");
+}
+
+//文件读入；
+void readInfoFromFile(const char* fileName, struct Node* listHeadNode) {
+	FILE* fp = fopen(fileName, "r");
+
+	if (fp == NULL) {
+		fp = fopen(fileName, "w");
+	}
+
+	struct student tempdata;
+	while (fscanf(fp, "%s %s %s %d %f %f %f\n", tempdata.num, tempdata.name, tempdata.sex, &tempdata.cla, &tempdata.Chinese, &tempdata.Math, &tempdata.English) != EOF) {
+		insertNodeByHead(listHeadNode, tempdata);
+		memset(&tempdata, 0, sizeof(tempdata));
+	}
+
+	fclose(fp);
+
+}
+
+//文件保存；
+void saveInfoFromFile(const char* fileName,struct Node* listHeadNode) {
+	FILE* fp = fopen(fileName, "w");
+	struct Node* pMove = listHeadNode->next;
+
+	while (pMove) {
+		fprintf(fp, "%s\t%s\t%s\t%d\t%.2f\t%.2f\t%.2f\n", pMove->data.num, pMove->data.name, pMove->data.sex, pMove->data.cla,
+			pMove->data.Chinese, pMove->data.Math, pMove->data.English);
+		    pMove = pMove->next;
+	}
+
+	fclose(fp);
+}
